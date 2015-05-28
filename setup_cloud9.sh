27955cf100d3200e06d7ff6df859d2268f2afce7
Daniel Boline <daniel.boline@stonybrook.edu>
2015-05-28 16:48:34 -0400

    ...

--- setup_cloud9.sh
+++ setup_cloud9.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# sudo apt-get install -y ipython python-matplotlib python-sklearn python-pandas htop
+sudo apt-get install -y ipython python-matplotlib python-sklearn python-pandas htop
 
 ### a horrible hack...
 sudo cp -a ~/.ssh /root/
 sudo bash -c "echo deb ssh://ddboline@ddbolineathome.mooo.com/var/www/html/deb/trusty/nltk_gensim ./ > /etc/apt/sources.list.d/py2deb2.list"
 sudo apt-get update
 
-sudo apt-get install -y --force-yes python-nltk python-gensim ipython python-matplotlib python-sklearn python-pandas htop
+sudo apt-get install -y --force-yes python-nltk python-gensim
 
 CURDIR=`pwd`
 cd $HOME
