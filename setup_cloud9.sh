c4a5ecf7ddfe8305fdc5c039a066656d4832afbd
Daniel Boline <daniel.boline@stonybrook.edu>
2015-05-28 17:46:18 -0400

    ...

--- setup_cloud9.sh
+++ setup_cloud9.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 
+sudo apt-get update
 sudo apt-get install -y ipython python-matplotlib python-sklearn python-pandas htop
 
-### a horrible hack...
 sudo cp -a ~/.ssh /root/
-sudo bash -c "echo deb ssh://ddboline@ddbolineathome.mooo.com/var/www/html/deb/trusty/nltk_gensim ./ > /etc/apt/sources.list.d/py2deb2.list"
+sudo bash -c "echo deb ssh://ddboline@ddbolineathome.mooo.com/var/www/html/deb/trusty/xgboost ./ > /etc/apt/sources.list.d/py2deb2.list"
 sudo apt-get update
 
-sudo apt-get install -y --force-yes python-nltk python-gensim
+sudo apt-get install -y --force-yes xgboost python-xgboost
 
-CURDIR=`pwd`
-cd $HOME
-scp ddboline@ddbolineathome.mooo.com:~/nltk_data_full.tar.gz .
-tar zxvf nltk_data_full.tar.gz
-rm nltk_data_full.tar.gz
-cd $CURDIR
+### a horrible hack...
+# sudo cp -a ~/.ssh /root/
+# sudo bash -c "echo deb ssh://ddboline@ddbolineathome.mooo.com/var/www/html/deb/trusty/nltk_gensim ./ > /etc/apt/sources.list.d/py2deb2.list"
+# sudo apt-get update
+# 
+# sudo apt-get install -y --force-yes python-nltk python-gensim
+# 
+# CURDIR=`pwd`
+# cd $HOME
+# scp ddboline@ddbolineathome.mooo.com:~/nltk_data_full.tar.gz .
+# tar zxvf nltk_data_full.tar.gz
+# rm nltk_data_full.tar.gz
+# cd $CURDIR
