62dd9e4aa3ccda66ce99594d032ba6f0f3289cef
Daniel Boline <daniel.boline@stonybrook.edu>
2015-06-03 16:53:45 -0400

    ...

--- my_model.py
+++ my_model.py
@@ -5,10 +5,10 @@ Created on Fri May  1 10:09:24 2015
 
 @author: ddboline
 """
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-from __future__ import unicode_literals
+#from __future__ import absolute_import
+#from __future__ import division
+#from __future__ import print_function
+#from __future__ import unicode_literals
 
 import gzip
 import cPickle as pickle
